e349bfbafff1f4fed6bffb8badd4f044a82ac3e6
Mehebul02 <mehebul2123@gmail.com>
2024-02-14 10:45:47 +0600

    hidden added

---
+++ index.js
@@ -0,0 +1,9 @@
+function play(){
+    // Home Screen 
+    const homeScreen =document.getElementById('home_screen');
+    homeScreen.classList.add("hidden");
+
+    // play Ground
+    const playGround = document.getElementById('play-ground');
+    playGround.classList.remove('hidden')
+}
